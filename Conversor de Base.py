98432a9d1b27848151b6360b886b4e57d7653603
Gustavo Gabriel Pereira Biolcatti <43222626+GustavoBiolcatti@users.noreply.github.com>
2019-08-22 14:46:11 -0300

    Add files via upload

---
+++ Conversor de Base.py
@@ -0,0 +1,27 @@
+import math
+
+n = int(input("Digite o número a ser convertido: "))
+n_base = int(input("Digite a base do número: "))
+
+base2 = []
+
+verif = True
+
+i = 0
+
+if n_base == 10:
+    # DECIMAL PARA BINÁRIO
+    div = n
+    while True:
+        if div >= 2:
+            resto = div % 2
+            base2.append(math.trunc(resto))
+            
+            div = div / 2
+        else:
+            break
+
+    base2.append(math.trunc(div))
+    base2 = base2[::-1]
+    
+print(base2)
